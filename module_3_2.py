728992cf3b312a3d401112f3f7deaaa229e1126c
OlegMozolevskyi <35062546+Mozolik@users.noreply.github.com>
2025-10-25 20:16:39 +0300

    задача 2

---
+++ module_3_2.py
@@ -0,0 +1,9 @@
+import random
+
+def get_numbers_ticket(min, max, quantity): 
+    if min>0 and max <= 1000 and quantity <= max-min:
+        number_list = random.sample(range(min, max), quantity)
+        number_list.sort()
+        return number_list
+
+          
