d08209772a05063be6fa21f80b31bc21922dfa9b
OlegMozolevskyi <35062546+Mozolik@users.noreply.github.com>
2025-10-27 21:13:14 +0200

    Правка по задачи 1

--- module_3_1.py
+++ module_3_1.py
@@ -4,9 +4,9 @@ def get_days_from_today(date):
     try:
         data_type = datetime.strptime(date, "%Y-%m-%d")
     except ValueError:
-        return 0
+        return None
     
     data_today = datetime.today()
+    data_today = data_today.combine(data_today.date(), data_today.min.time())
     count_days = (data_type - data_today).days
     return count_days
-
